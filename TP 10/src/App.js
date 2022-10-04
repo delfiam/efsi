347dd26f7c455b2cc2997911f3e9ab3adfb88797
Delfina Munich <89984382+delfiam@users.noreply.github.com>
2022-10-04 11:57:04 -0300

    arreglando tp10 para comenzar

--- TP 10/src/App.js
+++ TP 10/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/styles.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbarf from './components/Navbar';
+import Footers from './components/Footer';
+import  Carrousel  from './components/Carrousel';
+import Cards from './components/Card'
+import Containers from './components/Container'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Navbarf></Navbarf>
+        <Carrousel></Carrousel>
+      <div className='container'>
+        <div>
+        
+        </div>
+        <div>
+          <Cards></Cards>
+        </div>
+        <hr className="mt-3 mb-3"/>
+        <div>
+          <Containers></Containers>
+        </div>
+      </div>
+      
+      <Footers></Footers>
+
     </div>
   );
 }
