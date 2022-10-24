c9c80422a85a2bf01cc750c55432f419092aba67
Delfina Munich <89984382+delfiam@users.noreply.github.com>
2022-10-24 08:55:08 -0300

    quienes somos hecho

--- TP 10/src/components/Presentacion.js
+++ TP 10/src/components/Presentacion.js
@@ -5,14 +5,17 @@ import {Container, Row, Col} from 'react-bootstrap'
 export default function Pres() 
 {
     return (
-    <Container style={{borderColor: 'black'}}>
+      <div >
+    <Container>
           <Row>
-        <Col>Miles Gonzalo Morales es un adolescente de ascendencia afroamericana y latina residente en Brooklyn; quien después de haber sido mordido por una araña genéticamente modificada diseñada por el némesis de Spider-Man, Norman Osborn, asumió la identidad de Spider-Man en el Universo Ultimate tras la muerte de Peter Parker.
+        <Col style={{margin: 100}}>Miles Gonzalo Morales es un adolescente de ascendencia afroamericana y latina residente en Brooklyn; quien después de haber sido mordido por una araña genéticamente modificada diseñada por el némesis de Spider-Man, Norman Osborn, asumió la identidad de Spider-Man en el Universo Ultimate tras la muerte de Peter Parker.
 
 Posteriormente, tras las Guerras Secretas y la destrucción del multiverso, Miles y su familia fueron desplazados a la nueva Tierra-616 y comparte el título de Spider-Man con el Peter Parker de este universo.</Col>
-        <Col><img src='https://www.pngmart.com/files/12/Miles-Morales-Spider-Man-PNG-HD.png'/></Col>
+        <Col> <img src='https://www.pngmart.com/files/12/Miles-Morales-Spider-Man-PNG-HD.png' style={{width: '50%'}}/></Col>
       </Row>
      
     </Container>
+    </div>
     )
 }
+
