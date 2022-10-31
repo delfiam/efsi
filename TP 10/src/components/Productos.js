4258201734f0ea35e743a34e436784cbd628634c
Delfina Munich <89984382+delfiam@users.noreply.github.com>
2022-10-31 08:30:32 -0300

    productos js

--- TP 10/src/components/Productos.js
+++ TP 10/src/components/Productos.js
@@ -1,1 +1,45 @@
 // id, npmbre. añadido (true, false), descripcion, foto
+
+const listadoproductos = [
+    {
+        id: 1,
+        nombre: 'Inodoro Love Live',
+        añadido: false,
+        descripcion: 'nico yazawa',
+        foto: 'https://pm1.narvii.com/6405/dd1d6f41743f37a6e49164c0c463d7cdcd7258cc_hq.jpg',
+        precio: 1000
+    },
+    {
+        id: 2,
+        nombre: 'Lavamanos',
+        añadido: false,
+        descripcion: 'LET THAT SINK IN!',
+        foto: 'https://f4.bcbits.com/img/a1985991742_10.jpg',
+        precio: 500
+    },
+    {
+        id: 3,
+        nombre: 'Secador',
+        añadido: false,
+        descripcion: 'mirai zura!!',
+        foto: 'https://i.pinimg.com/originals/cf/ed/e1/cfede100eb6242ccb8b07b344254f04a.gif',
+        precio: 2000
+    },
+    {
+        id: 4,
+        nombre: 'Bañadera',
+        añadido: false,
+        descripcion: 'bien ahí peter1!!1!',
+        foto: 'https://c.tenor.com/zQ5M9wFKPz0AAAAd/spider-man-handled-like-champ.gif',
+        precio: 3000
+    },
+    {
+        id: 5,
+        nombre: 'Inodoro Spider-Man',
+        añadido: false,
+        descripcion: 'para los campeones',
+        foto: 'http://cdn.shopify.com/s/files/1/0364/9614/6491/products/D_Q_NP_962987-MLA45092064266_032021-O.jpg?v=1614881190',
+        precio: 1000
+    }
+
+]
