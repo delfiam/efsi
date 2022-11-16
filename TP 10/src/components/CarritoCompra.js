c0c6ea0bf34a638a294ad6a47da4e89dc0b7201c
Delfina Munich <89984382+delfiam@users.noreply.github.com>
2022-11-16 08:39:30 -0300

    total precio just dropped

--- TP 10/src/components/CarritoCompra.js
+++ TP 10/src/components/CarritoCompra.js
@@ -5,6 +5,7 @@ import { listadoproductos } from './Productos';
 import { useState, useContext, useEffect } from 'react';
 import Button from 'react-bootstrap/Button';
 import ProductosContext from './Context';
+import Card from 'react-bootstrap/Card';
 export default function CarritoCompra() {
     const [carrito, setCarrito] = useContext(ProductosContext);
 
@@ -13,28 +14,38 @@ export default function CarritoCompra() {
         setCarrito(eliminado);
         console.log(carrito);
     }
+    const obtenerpreciototal = () => {
+        let total = 0;
+        carrito.forEach((producto) => {
+            total += producto.precio;
+        })
+        return total;
+    }
 
+  
     return (
-            <div>
-                {carrito.map((producto) => {
-                    producto.id = carrito.indexOf(producto);
-                    let imagen = producto.foto;
-                    let nombre = producto.nombre;
-                    let descripcion = producto.descripcion;
-                    let precio = producto.precio;
-                    return (
-                            <div className="card ">
-                                <img src={imagen} className="card-img-top" alt="..." />
-                                <div className="card-body">
-                                    <h5 className="card-title">{nombre}</h5>
-                                    <p className="card-text">{descripcion}</p>
-                                    <p className="card-text">Precio: ${precio}</p>
-                                    <Button variant="primary" onClick={() => eliminarProducto(producto.id)}>eliminarProducto</Button>
-                                </div>
+        <div>
+            {carrito.map((producto) => {
+                producto.id = carrito.indexOf(producto);
+                let imagen = producto.foto;
+                let nombre = producto.nombre;
+                let descripcion = producto.descripcion;
+                let precio = producto.precio;
+                return (
+                        <div className="card " style={{ width: '15rem' }}>
+                            <img src={imagen} className="card-img-top" alt="..." />
+                            <div className="card-body">
+                                <h5 className="card-title">{nombre}</h5>
+                                <p className="card-text">{descripcion}</p>
+                                <p className="card-text">Precio: ${precio}</p>
+                                <Button variant="primary" onClick={() => eliminarProducto(producto.id)}>Eliminar producto</Button>
                             </div>
-
-                    )
-                })}
+                        </div>
+                )
+            })}
+             <Card>
+                            <Card.Body style={{width: 50}}>{carrito.length ? 'Total:' + obtenerpreciototal() : 'No hay elementos en el carrito'  }</Card.Body>
+                        </Card>
         </div>
 
     );
